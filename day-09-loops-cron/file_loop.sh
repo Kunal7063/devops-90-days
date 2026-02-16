c77c8f8ed93d0463c963cabe47d1bd550c6feddd
Kunal7063 <kunaldas9679@outlook.com>
2026-02-16 17:38:03 +0000

    Loops & Cron Jobs

---
+++ day-09-loops-cron/file_loop.sh
@@ -0,0 +1,5 @@
+for file in *.sh
+do
+    echo "Found script: $file"
+done
+
---
+++ day-09-loops-cron/live_monitor.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+while true
+do
+    clear
+    echo "System Monitoring..."
+    free -h
+    df -h /
+    sleep 5
+done
